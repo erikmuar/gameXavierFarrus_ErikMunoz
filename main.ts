8ef9d4db0eb9cc80eb047437a00dd228365d4a7a
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2026-01-22 18:49:09 +0100

    Movimiento muñeco

--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
-tiles.loadMap(tiles.createSmallMap(tilemap`level3`))
-game.onUpdate(function () {
-    music.play(music.createSong(hex`00780004080200`), music.PlaybackMode.UntilDone)
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    if (tiles.tileIs(tiles.locationOfSprite(sprite), assets.tile`derecha`)) {
+        sprite.vy = 0
+        sprite.vx = enemigo_velocidad
+    } else if (tiles.tileIs(tiles.locationOfSprite(sprite), assets.tile`abajo1`)) {
+        sprite.vy = enemigo_velocidad
+        sprite.vx = 0
+    } else if (tiles.tileIs(tiles.locationOfSprite(sprite), assets.tile`izquierda0`)) {
+        sprite.vy = 0
+        sprite.vx = 0 - enemigo_velocidad
+    } else if (tiles.tileIs(tiles.locationOfSprite(sprite), assets.tile`arriba0`)) {
+        sprite.vy = 0 - enemigo_velocidad
+        sprite.vx = 0
+    } else if (tiles.tileIs(tiles.locationOfSprite(sprite), assets.tile`doblesentido`)) {
+        sprite.vy = 0
+        sprite.vx = enemigo_velocidad
+        if (Math.percentChance(50)) {
+            sprite.vx = 0 - enemigo_velocidad
+        }
+    } else {
+    	
+    }
+})
+let nuevo_enemigo: Sprite = null
+let enemigo_velocidad = 0
+tiles.loadMap(tiles.createSmallMap(tilemap`map`))
+enemigo_velocidad = 50
+game.onUpdateInterval(500, function () {
+    nuevo_enemigo = sprites.create(assets.image`muñeco`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(nuevo_enemigo, assets.tile`myTile`)
+    nuevo_enemigo.vy = enemigo_velocidad
 })
